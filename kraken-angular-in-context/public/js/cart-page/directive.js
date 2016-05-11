201407ff68605d188919e96cd05f11afe5cbe294
Nate Sanders <ppnsanders@users.noreply.github.com>
2016-05-11 16:03:09 -0700

    Revert "feat(callbacks): cart page uses callbacks now"

--- kraken-angular-in-context/public/js/cart-page/directive.js
+++ kraken-angular-in-context/public/js/cart-page/directive.js
@@ -3,41 +3,27 @@ angular.module('frontEndApp').directive('cartPage', [function() {
     return {
         restrict: 'E',
         scope: {},
-        controller: ['$scope', 'angularLoad', '$location', '$timeout', 'apiModel', function($scope, angularLoad, $location, $timeout, apiModel) {
+        controller: ['$scope', 'angularLoad', '$location', 'apiModel', function($scope, angularLoad, $location, apiModel) {
             $scope.model = apiModel;
-
-            function doCheckout() {
-                paypal.checkout.initXO();
-                var createPayment = $scope.model.createPayment();
-                createPayment.success(function (response) {
-                    console.log('createPayment Success: ', response);
-                    paypal.checkout.startFlow(response.token);
-                });
-                createPayment.error(function (err) {
-                    console.error('createPayment ERROR ', err);
-                    paypal.checkout.closeFlow();
-                });
-            }            
-            
             angularLoad.loadScript('https://www.paypalobjects.com/api/checkout.js').then(function() {
-                paypal.checkout.setup("MFUX86KBB6EM2", {
-                    async: true,
+                function doCheckout() {
+                    paypal.checkout.initXO();
+                    var createPayment = $scope.model.createPayment();
+                    createPayment.success(function (response) {
+                        console.log('createPayment Success: ', response);
+                        paypal.checkout.startFlow(response.token);
+                    });
+                    createPayment.error(function (err) {
+                        console.error('createPayment ERROR ', err);
+                        paypal.checkout.closeFlow();
+                    });
+                }
+                console.log('checkout.js loaded');
+                paypal.checkout.setup('MFUX86KBB6EM2', {
                     environment: 'sandbox',
-                    buttons: [
-                        {
-                            container: 'paypalForm',
-                            type: 'checkout',
-                            shape: 'rect',
-                            size: 'small',
-                            click: doCheckout,
-                            color: 'blue'
-                        }
-                    ]
+                    container: 'paypalForm',
+                    click: doCheckout
                 });
-                paypal.checkout.events.on('success', function (token) { console.log('success, token: ', token)});
-                paypal.checkout.events.on('failure', function (error) { console.error('error! ', error)});
-                paypal.checkout.events.on('return', function (url) { console.log('return url: ', url)});
-
             }).catch(function (error) {
                 console.log('ERROR: When Rendering Checkout with PayPal Button - ');
                 console.log(error);
